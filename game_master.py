5a6ecb8e2535eddb57c3999f9a9856625e3057a1
bkim515dev <bkim515dev@gmail.com>
2022-02-05 11:08:46 -0800

    Initial Game Master file

---
+++ game_master.py
@@ -0,0 +1,20 @@
+"""
+Main Python function for running the Minesweeper game. Helps create the game
+board and interact with user through input.
+"""
+
+# Helps interact with the player and the board.
+class GameMaster:
+
+    def __init__(self):
+        pass
+
+    def get_game_type(self):
+        pass
+
+    def start_game(self):
+        pass
+
+game_master = GameMaster()
+game_master.get_game_type()
+game_master.start_game()
