0c4d0f122c26dfb599f6cf44ea16995bba0fe195
bkim515dev <bkim515dev@gmail.com>
2022-02-05 20:54:36 -0800

    Started board implementation. Better user input.

--- board.py
+++ board.py
@@ -4,29 +4,72 @@ Board class that controlls Minesweeper board.
 
 from enum import Enum
 
+
 class GameResult(Enum):
     STILL_RUNNING = 1
     WON = 2
     DEFEAT = 3
 
 
+class UserAction(Enum):
+    CLICK = 1
+    PLANT_FLAG = 2
+
+
+class Square:
+
+    def __init__(self, x_cord, y_cord):
+        self.x_cord = x_cord
+        self.y_cord = y_cord
+        self.has_mine = False
+        self.true_num_flags = 0
+        self.user_clicked = False
+        self.user_guess_flags = 0
+
+
 # Minesweeper Board. Represents the entire board, including each of its
 # individual squares.
 class MinesweeperBoard:
 
-    def __init__(self, board_length, board_height, num_mines):
-        self.board_length = board_length
-        self.board_height = board_height
+    def __init__(self):
+        self.num_xs = None
+        self.num_ys = None
+        self.num_mines = None
+        self.squares = {}
+
+    def set_up(self, num_xs, num_ys, num_mines):
+        self.num_xs = num_xs
+        self.num_ys = num_ys
         self.num_mines = num_mines
 
-    def display_board(self):
+        self.print_line_break()
+        print("Setting up [%s X %s] Board with # %s Mines" %
+              (num_xs, num_ys, num_mines))
+
+        for x in range(self.num_xs):
+            self.squares[x] = {}
+            for y in range(num_ys):
+                self.squares[x][y] = Square(x, y)
+
+    def is_set_up(self):
+        return (self.num_xs is not None and
+                self.num_ys is not None and
+                self.num_mines is not None)
+
+    def print_line_break(self):
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+    def print_board(self):
         print("-------------------")
+
+    def print_debug_board(self):
+        print("+_+_+_+_+_+_+_+_+_+_")
 
     def game_result(self):
         return GameResult.STILL_RUNNING
 
-    def is_valid_move(self, x_cord, y_cord, num_flags):
+    def is_valid_move(self, x_cord, y_cord, action):
         return (False, "Error")
 
-    def play_move(self, x_cord, y_cord, num_flags):
+    def play_move(self, x_cord, y_cord, action):
         pass
--- game_master.py
+++ game_master.py
@@ -14,23 +14,26 @@ class GameMaster:
 
     def start_game(self):
         # Initialize board with the given input.
-        (board_l, board_h, num_mines) = self.get_game_type()
-        self.board = board.MinesweeperBoard(board_l, board_h, num_mines)
+        self.board = board.MinesweeperBoard()
+
+        while not self.board.is_set_up():
+            (num_xs, num_ys, num_mines) = self.get_game_type()
+            self.board.set_up(num_xs, num_ys, num_mines)
 
         # While game is still running, get next move from user, then try to play
         # that move.
         while self.board.game_result() == board.GameResult.STILL_RUNNING:
-            self.board.display_board()
-            (x_cord, y_cord, num_flags) = self.get_next_move()
+            self.board.print_board()
+            (x_cord, y_cord, action) = self.get_next_move()
             (is_valid_move, err_msg) = self.board.is_valid_move(
-                x_cord, y_cord, num_flags)
+                x_cord, y_cord, action)
 
             if not is_valid_move:
-                print("Invalid Move at x-cord: %s - y-cord: %s" % (x_cord, y_cord))
-                print(" Error Message: %s" % err_msg)
+                print("\nInvalid Move at x-cord: %s - y-cord: %s" % (x_cord, y_cord))
+                print(" *Error Message: %s" % err_msg)
                 continue
 
-            self.board.play_move(x_cord, y_cord, num_flags)
+            self.board.play_move(x_cord, y_cord, action)
 
         # If the last move resulted in WON / DEFEAT, end the game.
         if self.board.game_result() == board.GameResult.WON:
@@ -45,37 +48,61 @@ class GameMaster:
     # Gets input from user and returns tuple of (length, height, num_mines).
     def get_game_type(self):
         # TODO(bonghyun): Add for custom.
-        print("Enter Grid Type - ('Beginner', 'Medium', 'Expert')")
-        game_type = input("'Beginner', 'Medium', 'Expert': ")
-        if game_type == 'Beginner':
-            self.max_x_cord = 10
-            self.max_y_cord = 10
-            self.num_mines = 10
-        elif game_type == 'Medium':
-            self.max_x_cord = 16
-            self.max_y_cord = 16
-            self.num_mines = 40
-        elif game_type == 'Expert':
-            self.max_x_cord = 16
-            self.max_y_cord = 30
-            self.num_mines = 99
-        else:
-            raise ValueError("Unexpected game_type: %s" % game_type)
-        return (self.max_x_cord, self.max_y_cord, self.num_mines)
+        while True:
+            print("Enter Grid Type - ('Beginner', 'Medium', 'Expert')")
+            game_type = input("'Beginner', 'Medium', 'Expert': ")
+            if game_type == 'Beginner':
+                return (9, 9, 10)
+            elif game_type == 'Medium':
+                return (16, 16, 40)
+            elif game_type == 'Expert':
+                return (16, 30, 99)
+            else:
+                print("**Unexpected Game Type: %s" % game_type)
+
 
     # Gets input from user for the next move. Should give x_cord, y_cord, and
     # num_flags (0 to indicate no mine).
     def get_next_move(self):
-        print("Enter x coordinate - (1 .. %s)" % self.max_x_cord)
-        x_cord = input("1 .. %s: " % self.max_x_cord)
+        while True:
+            print("Enter x coordinate - (0 .. %s)" % (self.board.num_xs - 1))
+            x_cord = input("0 .. %s: " % (self.board.num_xs - 1))
+            try:
+                x_cord = int(x_cord)
+            except:
+                print("%s is not a valid int." % x_cord)
+                continue
+            if x_cord < 0 or x_cord >= self.board.num_xs:
+                print("%s is out of bounds." % x_cord)
+            else:
+                break
 
-        print("Enter y coordinate - (1 .. %x)" % self.max_y_cord)
-        y_cord = input("1 .. %s: " % self.max_y_cord)
+        while True:
+            print("Enter y coordinate - (0 .. %s)" % (self.board.num_ys - 1))
+            y_cord = input("0 .. %s: " % (self.board.num_ys - 1))
+            try:
+                y_cord = int(y_cord)
+            except:
+                print("%s is not a valid int." % y_cord)
+                continue
+            if y_cord < 0 or y_cord >= self.board.num_ys:
+                print("%s is out of bounds." % y_cord)
+            else:
+                break
 
-        print ("Enter Number of Flags. Enter 0 for no mines")
-        num_flags = input("0 .. %s: " % self.num_mines)
+        while True:
+            print("Enter C to click (is safe). Enter F to plant flag (there's a mine).")
+            action_input = input("C or F: ")
+            if action_input == "C":
+                action = board.UserAction.CLICK
+                break
+            elif action_input == "F":
+                action = board.UserAction.PLANT_FLAG
+                break
+            else:
+                print("%s it not a valid action." % action_input)
 
-        return (x_cord, y_cord, num_flags)
+        return (x_cord, y_cord, action)
 
 
 game_master = GameMaster()
